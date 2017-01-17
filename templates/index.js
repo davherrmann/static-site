f82f2664a4c049ceff5cfc1d4d6fcbd6e80cf94d
David Herrmann <davherrmann@googlemail.com>
2017-01-17 21:11:05 +0100

    add viewport meta data in head

--- templates/index.js
+++ templates/index.js
@@ -2,6 +2,7 @@ const header = data => `
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
 
     <base href="${data.meta.baseUrl}">
 
